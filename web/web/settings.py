b395f5ed2aeb21e0f21a55da185fa0c9020ebcaa
dldkddkd <jisoolee1997@naver.com>
2024-12-30 11:49:00 +0900

    REC-25 docs: settings.py
    postgresql 연동

--- web/web/settings.py
+++ web/web/settings.py
@@ -109,10 +109,21 @@ WSGI_APPLICATION = "web.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',  # 데이터베이스 이름
+        'USER': 'postgres',       # PostgreSQL 사용자 이름
+        'PASSWORD': '1234',   # PostgreSQL 사용자 비밀번호
+        'HOST': '127.0.0.1',           # 또는 DB 서버 IP
+        'PORT': '5432',                # 기본값
     }
 }
 
