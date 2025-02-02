00563e708e7446fc2ea5a5f8023858c7bbf3e644
BH1107 <hiantonio1107@gmail.com>
2025-02-03 00:05:37 +0900

    REC-32 feat: 일정변경 프롬포트 언어별 작성, 일정변경 루트 조건 추가, 일정 생성 프롬포트 보완, 리트리버 중복 검색 제거

--- langgraph/chain_model/schedule_change_chain.py
+++ langgraph/chain_model/schedule_change_chain.py
@@ -1,12 +1,21 @@
 from prompts.prompt_schedule_change import prompt_schedule_change
+from prompts.prompt_schedule_change_chi import prompt_schedule_change_chi
+from prompts.prompt_schedule_change_eng import prompt_schedule_change_eng
+from prompts.prompt_schedule_change_ja import prompt_schedule_change_ja
 
 from langchain_openai import ChatOpenAI
 from langchain_core.output_parsers import StrOutputParser
 
 
-def schedule_change_chain():  
-    change_prompt = prompt_schedule_change
-
+def schedule_change_chain(language):  
+    if language == '한국어':
+      change_prompt = prompt_schedule_change
+    elif language == '영어':
+      change_prompt = prompt_schedule_change_eng
+    elif language == '중국어':
+      change_prompt = prompt_schedule_change_chi
+    elif language == '일본어':
+      change_prompt = prompt_schedule_change_ja
     # LLM
     change_model = ChatOpenAI(model_name="gpt-4o-mini",
                         temperature=0, 
